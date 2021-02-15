c7188d1c2d2009ee0afc96472ff6d8624a815331
Adrián Martinez <adrimrtnz@gmail.com>
2021-02-15 10:55:26 +0100

    Setting up the main window

---
+++ 2.SFML_2D/SFMLProject/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML\Graphics.hpp>
+//#include "SFML-2.5.1/include/SFML/Graphics.hpp"
+
+// Window Parameters
+const int S_WIDTH = 1024;
+const int S_HEIGHT = 768;
+
+sf::Vector2f viewSize(S_WIDTH, S_HEIGHT);
+sf::VideoMode vm(viewSize.x, viewSize.y);
+sf::RenderWindow window(vm, "SFLM Project", sf::Style::Default);
+
+int main(void) {
+	
+	// Initialize Game Object
+	while (window.isOpen()) {
+		
+		// Handle Keyboard Events
+		// Update Game Objects in the scene
+		// Render Game Objects
+	}
+}
