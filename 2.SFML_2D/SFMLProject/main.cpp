792e2b0adc6e2bdfe265386e8ca16877b80ac32d
Adrián Martinez <adrimrtnz@gmail.com>
2021-02-15 10:58:44 +0100

    Setting Up main window

--- 2.SFML_2D/SFMLProject/main.cpp
+++ 2.SFML_2D/SFMLProject/main.cpp
@@ -7,7 +7,7 @@ const int S_HEIGHT = 768;
 
 sf::Vector2f viewSize(S_WIDTH, S_HEIGHT);
 sf::VideoMode vm(viewSize.x, viewSize.y);
-sf::RenderWindow window(vm, "SFLM Project", sf::Style::Default);
+sf::RenderWindow window(vm, "SFLM Project!", sf::Style::Default);
 
 int main(void) {
 	
