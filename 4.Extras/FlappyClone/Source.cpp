4834c6309280187aa17a1e514fb39d3ebbf80fc2
Adrián Martinez <adrimrtnz@gmail.com>
2021-02-23 11:37:30 +0100

    Extra Project Setup

---
+++ 4.Extras/FlappyClone/Source.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+
+// Window Parameters
+const int S_WIDTH = 1024;
+const int S_HEIGHT = 768;
+
+sf::Vector2f viewSize(S_WIDTH, S_HEIGHT);
+sf::VideoMode vm(viewSize.x, viewSize.y);
+sf::RenderWindow window(vm, "FlappyClone", sf::Style::Default);
+
+int main(void)
+{
+	while (window.isOpen() {
+
+	}
+}
